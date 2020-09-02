f9e188523763df0c5f27447d98a56310981cf8d9
eriBloo <19932449+EriBloo@users.noreply.github.com>
2020-09-02 09:48:15 +0200

    Added functions for createing form to add new books + some minor changes

--- script.js
+++ script.js
@@ -12,8 +12,57 @@ function addBookToLibrary(title, author, pages, read = false) {
 	library.push(new Book(title, author, pages, read));
 }
 
+function addBook(e) {
+	const bookForm = document.createElement("form");
+	const book = document.createElement("div");
+
+	createForm(bookForm);
+
+	container.insertBefore(bookForm, e.target);
+
+	book.classList.add("book");
+	book.setAttribute("data", library.length);
+}
+
+function createForm(form) {
+	form.classList.add("book");
+
+	const inputTitle = document.createElement("input");
+	inputTitle.classList.add("form-title");
+	inputTitle.setAttribute("placeholder", "Title");
+	inputTitle.setAttribute("type", "text");
+	inputTitle.setAttribute("autocomplete", "off");
+	form.appendChild(inputTitle);
+
+	const inputAuthor = document.createElement("input");
+	inputAuthor.setAttribute("placeholder", "Author");
+	inputAuthor.setAttribute("type", "text");
+	inputAuthor.setAttribute("autocomplete", "off");
+	form.appendChild(inputAuthor);
+
+	const inputPages = document.createElement("input");
+	inputPages.setAttribute("placeholder", "Pages");
+	inputPages.setAttribute("type", "text");
+	inputPages.setAttribute("autocomplete", "off");
+	form.appendChild(inputPages);
+
+	const confirm = document.createElement("h3");
+	confirm.textContent = "+";
+	confirm.addEventListener("click", createBook);
+	form.appendChild(confirm);
+}
+
+function createBook() {
+	const form = document.querySelector("form");
+	const inputs = form.querySelectorAll("input");
+	
+	library.push(new Book(inputs[0].value, inputs[1].value, inputs[2].value));
+	
+	container.removeChild(form);
+}
+
 let library = [];
 
-addBookToLibrary("Hobbit", "Tolkien", 299);
-
-console.log(library)
+const container = document.querySelector(".container");
+const addButton = document.querySelector(".add");
+addButton.addEventListener("click", addBook); 
